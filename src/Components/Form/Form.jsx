19b37ae6283672dd49a41880be94d2e6db05658a
Vsord <stanislav060977@gmail.com>
2022-07-29 13:30:44 -0400

    form changed

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,43 +1,80 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import styles from './Form.module.scss'
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom'
 
 const Form = () => {
-    const [state, setState] = useState({
-        value: '',
-    });
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailErrorMessage, setEmailErrorMessage] = useState('');
+    const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+    const [isValid, setIsValid] = useState(false);
 
-    const stateChange = (e) => {
-        setState({
-            value: e.target.value
-        });
+    useEffect(() => {
+        if (emailErrorMessage === '' && passwordErrorMessage === '') {
+            setIsValid(false)
+        } else {
+            setIsValid(true)
+        }
+    }, [emailErrorMessage, passwordErrorMessage]);
+
+
+    const emailStateChange = (e) => {
+        setEmail(e.target.value);
+        const emailRegExp = /^(.+)@(.+)$/
+        if (!emailRegExp.test(e.target.value)) {
+            setEmailErrorMessage("Invalid email: Must contain a '@' symbol");
+        } else {
+            setEmailErrorMessage('');
+        }
+    };
+
+    const passwordStateChange = (e) => {
+        setPassword(e.target.value);
+        if (e.target.value.length < 4 || e.target.value.length > 9 ) {
+            setPasswordErrorMessage('Password must contain more than 4 and less than 9');
+        } else {
+            setPasswordErrorMessage('');
+        }
     };
 
     const formSubmit = (e) => {
-        console.log(`USER's data: ${state.value}`);
         e.preventDefault();
     };
 
-
     return (
-        <div className={styles.formWrapper}>
-            <form className={styles.formWrapper_form} onSubmit={formSubmit}>
-                <div className={styles.formWrapper_email}>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" onChange={stateChange} />
-                </div>
-                <div className={styles.formWrapper_password}>
-                    <label htmlFor="password">Password:</label>
-                    <input type="text" id="password" onChange={stateChange} />
-                </div>
-                <div className={styles.formWrapper_logInButton}>
-                    <NavLink to='/news'>
-                        <button type="submit">Log In</button>
-                    </NavLink>
-                </div>
-            </form>
-        </div>
+        <form className={styles.formWrapper_form} onSubmit={formSubmit}>
+            <div className={styles.formWrapper_email}>
+                <div className={styles.email_error}>{emailErrorMessage}</div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    name="email"
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={emailStateChange}
+                    placeholder='Type your email...'
+                    required
+                />
+            </div>
+            <div className={styles.formWrapper_password}>
+                <div className={styles.password_error}>{passwordErrorMessage}</div>
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={passwordStateChange}
+                    placeholder='Type your password...'
+                    required
+                />
+            </div>
+            <div className={styles.formWrapper_logInButton}>
+                <NavLink to='/news'>
+                    <button type="submit" disabled={isValid}>Log In</button>
+                </NavLink>
+            </div>
+        </form>
     );
 };
 
