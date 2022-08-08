8d869a18b578b07c9beee6369e7402a90c9e911c
Vsord <stanislav060977@gmail.com>
2022-08-07 22:47:22 -0700

    'title-is-able-to-be-changed'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import HookRequest from './Components/HookRequest/HookRequest'
+import Data from './Components/Data/Data'
+
 
 function App() {
+    const [state, setState] = useState({
+        isLoaded: false,
+        dataArray: [],
+    });
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(res => {
+                if (res.status >= 200 && res.status < 300) {
+                    return res.json();
+                } else {
+                    let err = new Error('Something went wrong..');
+                    throw err
+                };
+            })
+            .then(data => setState({
+                isLoaded: true,
+                dataArray: data,
+            }))
+            .catch(error => {
+                console.error(error.message);
+            });
+    }, []);
+
+    const { isLoaded, dataArray } = state;
+    if (!isLoaded) {
+        return (
+            <div className='preloader'>
+                Loading...
+            </div>
+        );
+    };
   return (
     <div className="App">
-      <HookRequest />
+        <Data data={dataArray} />
     </div>
   );
 }
--- src/Components/Data/Data.jsx
+++ src/Components/Data/Data.jsx
@@ -1,24 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 import './Data.css'
 
 const Data = (props) => {
+    const [title, setTitle] = useState(props.data.map(obj => obj.id <= 1 ? obj.title : null));
+    const [body, setBody] = useState(props.data.map(obj => obj.id <= 1 ? obj.body : null));
+
+    let dataTextArea = () => {
+        return (
+            <textarea
+                className='textArea'
+                defaultValue={innerText}
+                onChange={changeInnerText}
+            />
+        )
+    };
+    const [innerText, setInnerText] = useState(title);
     let dataList = props.data.map(obj => {
-        if (obj.id <= 10) {
+        if (obj.id <= 1) {
             return (
-                < li key={obj.id} className="list" >
-                    <h3>{obj.title}</h3>
-                    <p>{obj.body}</p>
-                    <p>{obj.id}</p>
-                </li >
+                < li key={obj.id} className="list">
+                    <h3>{innerText}</h3>
+                    <p>{body}</p>
+                </li>
             )
         }
     });
 
+
+
+    const [data, setData] = useState(dataList);
+
+
+    const changeInnerText = (e) => {
+        setInnerText(e.target.value)
+    }
+
+    const addTextArea = () => {
+        setData(dataTextArea())
+    }
+
+    const saveTextAreaData = () => {
+        setTitle(innerText)
+        setData(dataList)
+        console.log(innerText)
+    }
+
+
     return (
         <div>
             <ul>
-                {dataList}
+                {data}
             </ul>
+            <div className='textArea_button_block'>
+                <button onClick={addTextArea} className='textArea_edit_botton'>Edit</button>
+                <button onClick={saveTextAreaData} className='textArea_save_botton'>Save</button>
+            </div>
         </div>
     );
 };
